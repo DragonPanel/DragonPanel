8e5669162075e9428845736abe7fd39aa1509675
ScuroGuardiano <scuroguardiano@users.noreply.github.com>
2023-01-27 03:18:06 +0100

    Genesis - monorepo structure

---
+++ packages/api/src/main.ts
@@ -0,0 +1,11 @@
+import { hello } from '@dragon-panel/dto';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  hello();
+
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
