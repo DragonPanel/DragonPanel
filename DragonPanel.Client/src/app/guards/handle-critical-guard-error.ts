e3077205e0465f820bed108bcd9591e020763641
ScuroGuardiano <scuroguardiano@users.noreply.github.com>
2024-08-07 16:53:37 +0200

    added some initial api docs

--- DragonPanel.Client/src/app/guards/handle-critical-guard-error.ts
+++ DragonPanel.Client/src/app/guards/handle-critical-guard-error.ts
@@ -2,7 +2,8 @@ import { Router } from "@angular/router";
 import { of } from "rxjs";
 
 export function handleCriticalGuardError(router: Router, name: string, error: any) {
-  console.error(`------- CRITICAL GUARD ERROR -------`)
+  console.error(`------- CRITICAL GUARD ERROR -------`);
+  console.error(`Guard: ${name}`);
   console.error(error);
   return of(router.parseUrl("/panic"));
 }
